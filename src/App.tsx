995aa7ffd0f2d8134c83ad6b1027ea84f505da46
janeluck <janeluck158@gmail.com>
2019-05-04 10:30:07 +0800

    chore: 添加ts配置

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          Awesome TypeScript
         </p>
         <a
           className="App-link"
